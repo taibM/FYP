ca9ee5cb4dbe06910b36eace61b5b5df68587422
SuperBatata <47794586+SuperBatata@users.noreply.github.com>
2019-11-27 12:01:28 +0100

    Add files via upload

--- authenticate.php
+++ authenticate.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'fyp';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
@@ -15,7 +15,7 @@ if (!isset($_POST['username'], $_POST['password'])) {
 	// Could not get the data that should have been sent.
 	die('Please fill both the username and password field!');
 } // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT id_client, password FROM client WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
@@ -33,7 +33,7 @@ if ($stmt->num_rows > 0) {
 		session_regenerate_id();
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['username'];
-		$_SESSION['id'] = $id;
+		$_SESSION['id_client'] = $id;
 		header('Location: home.php');
 	} else {
 		echo 'Incorrect password!';
--- profile.php
+++ profile.php
@@ -9,15 +9,15 @@ if (!isset($_SESSION['loggedin'])) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'fyp';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT password, email FROM client WHERE id_client = ?');
 // In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
+$stmt->bind_param('i', $_SESSION['id_client']);
 $stmt->execute();
 $stmt->bind_result($password, $email);
 $stmt->fetch();
